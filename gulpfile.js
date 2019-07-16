cf1702ab1da2be6d2f35d7b13bbebe2509ada21e
spark <228436652@qq.com>
2019-07-16 18:02:21 +0800

    修复主题图片

--- gulpfile.js
+++ gulpfile.js
@@ -7,49 +7,60 @@ var autoprefixer = require('gulp-autoprefixer');
 var zip = require('gulp-zip');
 
 var swallowError = function swallowError(error) {
-  gutil.log(error.toString());
-  gutil.beep();
-  this.emit('end');
+    gutil.log(error.toString());
+    gutil.beep();
+    this.emit('end');
 };
 
 var nodemonServerInit = function() {
-  livereload.listen(1234);
+    livereload.listen(1234);
 };
 
 gulp.task('build', ['sass'], function() {
-  return nodemonServerInit();
+    return nodemonServerInit();
 });
 
 gulp.task('sass', function() {
-  return gulp
-    .src('assets/scss/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', swallowError))
-    .pipe(sourcemaps.init())
-    .pipe(
-      autoprefixer({
-        browsers: ['last 2 versions']
-      })
-    )
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('assets/built/'))
-    .pipe(livereload());
+    return gulp
+        .src('assets/scss/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', swallowError))
+        .pipe(sourcemaps.init())
+        .pipe(
+            autoprefixer({
+                browsers: [
+                    "last 3 versions",
+                    "ie >= 10",
+                    "ie_mob >= 10",
+                    "ff >= 30",
+                    "chrome >= 34",
+                    "safari >= 6",
+                    "ios >= 6",
+                    "android >= 4.4"
+                ]
+            })
+        )
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('assets/built/'))
+        .pipe(livereload());
 });
 
 gulp.task('watch', function() {
-  gulp.watch('assets/scss/**', ['sass']);
+    gulp.watch('assets/scss/**', ['sass']);
 });
 
 gulp.task('zip', ['sass'], function() {
-  var targetDir = 'dist/';
-  var themeName = require('./package.json').name;
-  var filename = themeName + '.zip';
+    var targetDir = 'dist/';
+    var themeName = require('./package.json').name;
+    var filename = themeName + '.zip';
 
-  return gulp
-    .src(['**', '!node_modules', '!node_modules/**', '!dist', '!dist/**'])
-    .pipe(zip(filename))
-    .pipe(gulp.dest(targetDir));
+    return gulp
+        .src(['**', '!node_modules', '!node_modules/**', '!dist', '!dist/**'])
+        .pipe(zip(filename))
+        .pipe(gulp.dest(targetDir));
 });
 
 gulp.task('default', ['build'], function() {
-  gulp.start('watch');
+    gulp.start('watch');
 });
